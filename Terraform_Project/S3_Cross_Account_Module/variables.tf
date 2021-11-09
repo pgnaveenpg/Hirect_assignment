97b472f3f1f8781a6c9073cdf4f0a2a5c9e2c50c
Naveen PG <66114170+pgnaveenpg@users.noreply.github.com>
2021-11-09 14:05:09 +0530

    Update variables.tf

--- Terraform_Project/S3_Cross_Account_Module/variables.tf
+++ Terraform_Project/S3_Cross_Account_Module/variables.tf
@@ -5,10 +5,11 @@ variable "name_prefix" {
 }
 
 # We can use list of accounts for multiple cross-account access 
-# variable "cross_account_users" {
-#  type = "list"
-#  default = [accountID1,accountID2,...]
-# }
+
+   variable "cross_account_ids" {
+   type = list
+   default = ["856866741119","064349442911"]
+ }
 
 # If we already have a s3 bucket  created for logging then we can directly use log bucket id 
 # variable "log_bucket_id" {
