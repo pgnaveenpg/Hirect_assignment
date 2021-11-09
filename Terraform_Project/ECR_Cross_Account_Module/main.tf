ca62821e03cdc1083aee4b1d28efd4e775aa3dee
Naveen PG <66114170+pgnaveenpg@users.noreply.github.com>
2021-11-09 22:16:42 +0530

    ECR_cross_account

--- Terraform_Project/ECR_Cross_Account_Module/main.tf
+++ Terraform_Project/ECR_Cross_Account_Module/main.tf
@@ -1,1 +1,42 @@
+resource "aws_ecr_repository" "repo" {
+  name = "${var.name}"
+}
 
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "repo" {
+  statement {
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:BatchGetImage",
+      "ecr:DescribeImageScanFindings",
+      "ecr:DescribeImages",
+      "ecr:DescribeRepositories",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetLifecyclePolicy",
+      "ecr:GetLifecyclePolicyPreview",
+      "ecr:GetRepositoryPolicy",
+      "ecr:ListImages",
+      "ecr:ListTagsForResource",
+      
+    ]
+    principals {
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
+        "arn:aws:iam::${element(var.cross_account_ids,1)}:root"
+      ]
+      type = "AWS"
+    }
+    sid     = "AllowReadonlyAccesstoCrossAccountUsers"
+  }
+}
+
+resource "aws_ecr_repository_policy" "repo" {
+  policy      = "${data.aws_iam_policy_document.repo.json}"
+  repository  = "${aws_ecr_repository.repo.name}"
+}
+
+resource "aws_ecr_lifecycle_policy" "repo" {
+  policy      = "${file("${path.module}/lifecycle-policy.json")}"
+  repository  = "${aws_ecr_repository.repo.name}"
+}
---
+++ Terraform_Project/ECR_Cross_Account_Module/variables.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  type = string
+  default = "assignment-repo"
+}
+
+ variable "cross_account_ids" {
+  type = list
+  default = ["856866741119","064349442911"]
+ }
